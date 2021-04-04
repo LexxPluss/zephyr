7b69cfe1ca9789a6ec721b6ab78cccb9128c0b3b
Atsunori Saito <sai@yedo.com>
2021-04-04 11:56:20 +0900

    Removed SPI definitions.

--- lexxpluss_apps/src/led_controller.cpp
+++ lexxpluss_apps/src/led_controller.cpp
@@ -1,13 +1,10 @@
 #include <zephyr.h>
-#include <device.h>
-#include <drivers/spi.h>
 #include "thread_runner.hpp"
 
 class led_controller {
 public:
     int init() {
-        dev = device_get_binding(DT_LABEL(DT_NODELABEL(spi4)));
-        return dev == nullptr ? -1 : 0;
+        return 0;
     }
     void run() {
         while (true) {
@@ -15,13 +12,6 @@ public:
         }
     }
 private:
-    const device *dev = nullptr;
-    const spi_config config = {
-        .frequency = 0,
-        .operation = SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8),
-        .slave = 0,
-        .cs = nullptr
-    };
 };
 
 LEXX_THREAD(led_controller);
