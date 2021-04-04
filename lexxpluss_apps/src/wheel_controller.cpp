b463c5d391cd86ce9093a777222f70d0f24e907e
Atsunori Saito <sai@yedo.com>
2021-04-04 11:54:39 +0900

    Fixed initialize of device.

--- lexxpluss_apps/src/wheel_controller.cpp
+++ lexxpluss_apps/src/wheel_controller.cpp
@@ -12,7 +12,7 @@
 class wheel_controller {
 public:
     int init() {
-        const device *dev = device_get_binding(LABEL);
+        dev = device_get_binding(LABEL);
         if (dev)
             gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAG);
         return dev == nullptr ? -1 : 0;
