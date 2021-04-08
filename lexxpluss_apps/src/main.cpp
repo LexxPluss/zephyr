531c21416be7f6d0085f94bb90838c7a08a3c394
Atsunori Saito <sai@yedo.com>
2021-04-08 21:42:51 +0900

    Moved LED2 test.

--- lexxpluss_apps/src/main.cpp
+++ lexxpluss_apps/src/main.cpp
@@ -1,8 +1,27 @@
 #include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+
+#define LED   DT_ALIAS(led2)
+#define LABEL DT_GPIO_LABEL(LED, gpios)
+#define PIN   DT_GPIO_PIN(LED, gpios)
+#define FLAG  DT_GPIO_FLAGS(LED, gpios)
 
 void main()
 {
-    while (true) {
-        k_msleep(1000);
+    const device *dev = device_get_binding(LABEL);
+    if (dev) {
+        gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAG);
+        while (true) {
+            gpio_pin_set(dev, PIN, 1);
+            k_msleep(300);
+            gpio_pin_set(dev, PIN, 0);
+            k_msleep(300);
+        }
+    } else {
+        while (true) {
+            k_msleep(1000);
+        }
     }
 }
--- lexxpluss_apps/src/sonar_controller.cpp
+++ lexxpluss_apps/src/sonar_controller.cpp
@@ -1,27 +1,15 @@
 #include <zephyr.h>
 #include <device.h>
 #include <devicetree.h>
-#include <drivers/gpio.h>
 #include "thread_runner.hpp"
-
-#define LED   DT_ALIAS(led2)
-#define LABEL DT_GPIO_LABEL(LED, gpios)
-#define PIN   DT_GPIO_PIN(LED, gpios)
-#define FLAG  DT_GPIO_FLAGS(LED, gpios)
 
 class sonar_controller {
 public:
     int init() {
-        dev = device_get_binding(LABEL);
-        if (dev)
-            gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAG);
-        return dev == nullptr ? -1 : 0;
+        return 0;
     }
     void run() {
         while (true) {
-            gpio_pin_set(dev, PIN, 1);
-            k_msleep(300);
-            gpio_pin_set(dev, PIN, 0);
             k_msleep(300);
         }
     }
