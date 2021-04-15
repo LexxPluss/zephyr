205cd1e1a93ff2e50326a2a420cc2c9f0a459f86
Atsunori Saito <sai@yedo.com>
2021-04-15 19:24:47 +0900

    Implemented sonar logic.

--- lexxpluss_apps/src/sonar_controller.cpp
+++ lexxpluss_apps/src/sonar_controller.cpp
@@ -1,19 +1,32 @@
 #include <zephyr.h>
 #include <device.h>
-#include <devicetree.h>
+#include <drivers/sensor.h>
+#include <logging/log.h>
 #include "thread_runner.hpp"
+
+LOG_MODULE_REGISTER(sonar_controller, LOG_LEVEL_INF);
 
 class sonar_controller {
 public:
     int init() {
-        return 0;
+        dev = device_get_binding("HC-SR04_0");
+        LOG_INF("SONAR controller for LexxPluss board. (%p)", dev);
+        return dev == nullptr ? -1 : 0;
     }
     void run() {
         while (true) {
-            k_msleep(300);
+            measure();
+            k_sleep(K_MSEC(100));
         }
     }
 private:
+    void measure() {
+        if (sensor_sample_fetch_chan(dev, SENSOR_CHAN_ALL) == 0) {
+            struct sensor_value distance;
+            if (sensor_channel_get(dev, SENSOR_CHAN_DISTANCE, &distance) == 0)
+                LOG_INF("%s: %d.%03dM", dev->name, distance.val1, (distance.val2 / 1000));
+        }
+    }
     const device *dev = nullptr;
 };
 
