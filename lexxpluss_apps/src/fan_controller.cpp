1065f8decdbcc784a4ac72a9f4dfa092a3c1a75a
Atsunori Saito <sai@yedo.com>
2021-05-20 15:40:15 +0900

    Fixed PWM fan controller.

--- lexxpluss_apps/src/fan_controller.cpp
+++ lexxpluss_apps/src/fan_controller.cpp
@@ -14,23 +14,17 @@ class fan_controller {
 public:
     int setup() {
         k_msgq_init(&fan_controller_msgq, fan_controller_msgq_buffer, sizeof (fan_message), 10);
-#if 0
-        pwm_pin_set_usec(dev[0], 1, 100, 10, 0);
-        pwm_pin_set_usec(dev[1], 1, 100, 10, 0);
         dev[0] = device_get_binding("PWM_10");
         dev[1] = device_get_binding("PWM_11");
-        return dev == nullptr ? -1 : 0;
-#else
-        return 0;
-#endif
+        pwm_pin_set_usec(dev[0], 1, 100, 10, 0);
+        pwm_pin_set_usec(dev[1], 1, 100, 10, 0);
+        return dev[0] == nullptr || dev[1] == nullptr ? -1 : 0;
     }
     void loop() const {
         fan_message message;
         if (k_msgq_get(&led_controller_msgq, &message, K_FOREVER) == 0) {
-#if 0
             pwm_pin_set_usec(dev[0], 1, PWM_USEC, message.duty_percent[0] * PWM_USEC / 100, 0);
             pwm_pin_set_usec(dev[1], 1, PWM_USEC, message.duty_percent[1] * PWM_USEC / 100, 0);
-#endif
         }
     }
 private:
