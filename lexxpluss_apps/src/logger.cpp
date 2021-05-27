fd23272923419f6a1e75e3fcfd674490d3b04774
Atsunori Saito <sai@yedo.com>
2021-05-27 19:13:59 +0900

    Supports MicroSD logger backend.

--- lexxpluss_apps/src/logger.cpp
+++ lexxpluss_apps/src/logger.cpp
@@ -2,6 +2,8 @@
 #include <disk/disk_access.h>
 #include <fs/fs.h>
 #include <ff.h>
+#include <logging/log_backend.h>
+#include <logging/log_output.h>
 #include <logging/log.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -63,6 +65,7 @@ public:
             LOG_INF("make log directory. (%s)", log_strdup(workpath));
         }
         logpath[0] = 0;
+        write_error_count = 0;
     }
     void maintain() {
         int32_t freeMB = get_freeMB(rootpath);
@@ -112,14 +115,20 @@ public:
             int n = atoi(num);
             snprintf(logpath, sizeof logpath, "%s/log/log%03u.log", rootpath, n + 1);
         }
+        output_enabled = true;
         LOG_INF("new log path %s", log_strdup(logpath));
     }
-    void output(const char* message) {
-        if (logpath[0] != 0 && message != nullptr && message[0] != 0) {
+    void output(const uint8_t* data, uint32_t length) {
+        if (output_enabled && logpath[0] != 0 && data != nullptr && length != 0) {
             fs_file_t fp;
             fs_file_t_init(&fp);
             if (fs_open(&fp, logpath, FS_O_WRITE | FS_O_CREATE | FS_O_APPEND) == 0) {
-                fs_write(&fp, message, strlen(message));
+                if (fs_write(&fp, data, length) != static_cast<ssize_t>(length)) {
+                    if (++write_error_count > 10) {
+                        output_enabled = false;
+                        LOG_INF("Too many write errors, disable output to MicroSD.");
+                    }
+                }
                 fs_close(&fp);
             }
         }
@@ -135,8 +144,51 @@ private:
     sorted_string_array entries;
     fs_dirent dirent;
     char rootpath[PATH_MAX], workpath[PATH_MAX], logpath[PATH_MAX];
+    uint32_t write_error_count = 0;
+    bool output_enabled = false;
     static constexpr int32_t MIN_FREE_MB = 512;
 };
+
+#define BUFSIZE 256
+uint8_t buf[BUFSIZE];
+log_manager logman;
+
+int char_out(uint8_t *buf, size_t size, void *ctx)
+{
+    logman.output(buf, size);
+    return size;
+}
+
+LOG_OUTPUT_DEFINE(log_output_microsd, char_out, buf, sizeof buf);
+
+void microsd_put(const struct log_backend *const backend,
+                        struct log_msg *msg)
+{
+    log_msg_get(msg);
+    uint32_t flags = LOG_OUTPUT_FLAG_LEVEL |
+                     LOG_OUTPUT_FLAG_TIMESTAMP |
+                     LOG_OUTPUT_FLAG_FORMAT_TIMESTAMP;
+    log_output_msg_process(&log_output_microsd, msg, flags);
+    log_msg_put(msg);
+}
+
+void microsd_dropped(const struct log_backend *const backend, uint32_t cnt)
+{
+    log_output_dropped_process(&log_output_microsd, cnt);
+}
+
+void microsd_panic(const struct log_backend *const backend)
+{
+    log_output_flush(&log_output_microsd);
+}
+
+const struct log_backend_api log_backend_microsd_api = {
+    .put = microsd_put,
+    .dropped = microsd_dropped,
+    .panic = microsd_panic,
+};
+
+LOG_BACKEND_DEFINE(log_backend_microsd, log_backend_microsd_api, true);
 
 class logger {
 public:
@@ -159,7 +211,6 @@ public:
         k_msleep(1000);
     }
 private:
-    log_manager logman;
     FATFS fatfs;
     fs_mount_t mount;
     static const char* mnt_point;
