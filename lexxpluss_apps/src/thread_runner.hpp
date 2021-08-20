733e6eca779ec86c4a66f77c306eb010cc90c547
Atsunori Saito <sai@yedo.com>
2021-08-20 19:08:27 +0900

    Added ROS sample.

---
+++ lexxpluss_apps/src/thread_runner.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <zephyr.h>
+
+#define LEXX_THREAD_RUNNER_DELAYED(name, delay) \
+    static name instance; \
+    K_THREAD_DEFINE(tid_##name, 2048, &thread_runner<name>::runner, &instance, nullptr, nullptr, 5, K_FP_REGS, delay)
+
+#define LEXX_THREAD_RUNNER(name) LEXX_THREAD_RUNNER_DELAYED(name, 1000)
+
+template <class T>
+struct thread_runner {
+    static void runner(void *p1, void *p2, void *p3) {
+        if (p1 != nullptr) {
+            T *target = static_cast<T*>(p1);
+            if (target->setup() == 0) {
+                while (true)
+                    target->loop();
+            }
+        }
+    }
+};
