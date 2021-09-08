95d81242bb88113e23144309aad5876c82783146
Atsunori Saito <sai@yedo.com>
2021-09-08 09:33:10 +0900

    Added the actuator driver. (in progress)

--- lexxpluss_apps/src/message.hpp
+++ lexxpluss_apps/src/message.hpp
@@ -36,6 +36,20 @@ struct msg_ros2pgv {
     uint8_t dir_command;
 } __attribute__((aligned(4)));
 
+struct msg_ros2actuator {
+    uint16_t data[3];
+    uint8_t type;
+    static constexpr uint8_t CWCCW = 0;
+    static constexpr uint8_t DUTY = 1;
+} __attribute__((aligned(4)));
+
+struct msg_actuator2ros {
+    int32_t encoder_count[3];
+    uint16_t current[3];
+} __attribute__((aligned(4)));
+
 extern k_msgq msgq_ros2led;
 extern k_msgq msgq_pgv2ros;
 extern k_msgq msgq_ros2pgv;
+extern k_msgq msgq_actuator2ros;
+extern k_msgq msgq_ros2actuator;
