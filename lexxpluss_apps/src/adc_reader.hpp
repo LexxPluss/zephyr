f2cd2cbacc80f33e77b3eb043f53acb47d3626d4
Atsunori Saito <sai@yedo.com>
2021-09-17 17:14:50 +0900

    Supports ADC.

---
+++ lexxpluss_apps/src/adc_reader.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+class adc_reader {
+public:
+    int setup();
+    void loop();
+    static uint16_t get(int index);
+    static constexpr int INDEX_DOWNWARD_L{0};
+    static constexpr int INDEX_DOWNWARD_R{1};
+    static constexpr int INDEX_ACTUATOR_0{2};
+    static constexpr int INDEX_ACTUATOR_1{3};
+    static constexpr int INDEX_ACTUATOR_2{4};
+    static constexpr int INDEX_CONNECT_CART{5};
+private:
+};
