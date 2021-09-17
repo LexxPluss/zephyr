2c2c57f648a56c8afd24d1552e6b1c98104e863e
Atsunori Saito <sai@yedo.com>
2021-09-17 17:09:44 +0900

    Fixed SoC type.

--- boards/arm/lexxpluss_mb01/board.cmake
+++ boards/arm/lexxpluss_mb01/board.cmake
@@ -1,4 +1,4 @@
-board_runner_args(jlink "--device=STM32F765BG" "--speed=4000")
+board_runner_args(jlink "--device=STM32F769BG" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
