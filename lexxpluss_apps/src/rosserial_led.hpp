ed89077d9107e6d3be2e10a1eb867c2b9dd6feaa
Atsunori Saito <sai@yedo.com>
2021-10-19 10:48:52 +0900

    Supports new LED message.

--- lexxpluss_apps/src/rosserial_led.hpp
+++ lexxpluss_apps/src/rosserial_led.hpp
@@ -3,6 +3,7 @@
 #include <zephyr.h>
 #include "ros/node_handle.h"
 #include "std_msgs/String.h"
+#include "lexxauto_msgs/Led.h"
 #include "led_controller.hpp"
 
 class ros_led {
@@ -31,17 +32,17 @@ private:
         while (k_msgq_put(&msgq_ros2led, &message, K_NO_WAIT) != 0)
             k_msgq_purge(&msgq_ros2led);
     }
-    void callback_direct(const std_msgs::String& req) {
+    void callback_direct(const lexxauto_msgs::Led& req) {
         msg_ros2led message;
         message.pattern = msg_ros2led::RGB;
-        message.rgb[0] = 0x00;
-        message.rgb[1] = 0x00;
-        message.rgb[2] = 0x00;
+        message.rgb[0] = req.r;
+        message.rgb[1] = req.g;
+        message.rgb[2] = req.b;
         while (k_msgq_put(&msgq_ros2led, &message, K_NO_WAIT) != 0)
             k_msgq_purge(&msgq_ros2led);
     }
     ros::Subscriber<std_msgs::String, ros_led> sub_string{"/body_control/led", &ros_led::callback_string, this};
-    ros::Subscriber<std_msgs::String/*@@*/, ros_led> sub_direct{"/body_control/led_direct", &ros_led::callback_direct, this};
+    ros::Subscriber<lexxauto_msgs::Led, ros_led> sub_direct{"/body_control/led_direct", &ros_led::callback_direct, this};
 };
 
 // vim: set expandtab shiftwidth=4:
