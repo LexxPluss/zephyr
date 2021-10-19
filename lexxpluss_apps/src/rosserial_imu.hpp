78bf34d2b6c79c9bde97bc3488c8f592d262d000
Atsunori Saito <sai@yedo.com>
2021-10-19 11:03:00 +0900

    Supports new IMU message.

--- lexxpluss_apps/src/rosserial_imu.hpp
+++ lexxpluss_apps/src/rosserial_imu.hpp
@@ -2,45 +2,35 @@
 
 #include <zephyr.h>
 #include "ros/node_handle.h"
-#include "geometry_msgs/Vector3.h"
+#include "lexxauto_msgs/Imu.h"
 #include "imu_controller.hpp"
 
 class ros_imu {
 public:
     void init(ros::NodeHandle &nh) {
-        gyro.init(nh);
-        accel.init(nh);
-        angle.init(nh);
-        velocity.init(nh);
+        nh.advertise(pub);
     }
     void poll() {
         msg_imu2ros message;
         if (k_msgq_get(&msgq_imu2ros, &message, K_NO_WAIT) == 0) {
-            gyro.poll(message.gyro);
-            accel.poll(message.accel);
-            angle.poll(message.delta_ang);
-            velocity.poll(message.delta_vel);
+            msg.gyro.x = message.gyro[0];
+            msg.gyro.y = message.gyro[1];
+            msg.gyro.z = message.gyro[2];
+            msg.accel.x = message.accel[0];
+            msg.accel.y = message.accel[1];
+            msg.accel.z = message.accel[2];
+            msg.ang.x = message.delta_ang[0];
+            msg.ang.y = message.delta_ang[1];
+            msg.ang.z = message.delta_ang[2];
+            msg.vel.x = message.delta_vel[0];
+            msg.vel.y = message.delta_vel[1];
+            msg.vel.z = message.delta_vel[2];
+            pub.publish(&msg);
         }
     }
 private:
-    class imupub {
-    public:
-        imupub(const char *name) : pub(name, &msg) {}
-        void init(ros::NodeHandle &nh) {nh.advertise(pub);}
-        void poll(const float *data) {
-            msg.x = data[0];
-            msg.y = data[1];
-            msg.z = data[2];
-            pub.publish(&msg);
-        }
-        ros::Publisher pub;
-    private:
-        geometry_msgs::Vector3 msg;
-    };
-    imupub gyro{"adis16470_gyro_data"};
-    imupub accel{"adis16470_accel_data"};
-    imupub angle{"adis16470_ang_data"};
-    imupub velocity{"adis16470_vel_data"};
+    lexxauto_msgs::Imu msg;
+    ros::Publisher pub{"imu", &msg};
 };
 
 // vim: set expandtab shiftwidth=4:
