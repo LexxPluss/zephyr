7e13f6b543f80037259e87a62e65017250a9e59b
Atsunori Saito <sai@yedo.com>
2021-10-26 17:38:24 +0900

    Modified ADC to single channel read.

--- lexxpluss_apps/src/adc_reader.cpp
+++ lexxpluss_apps/src/adc_reader.cpp
@@ -8,27 +8,13 @@ class adc_reader_impl {
 public:
     int init() {
         dev = device_get_binding("ADC_1");
-        if (dev != nullptr) {
-            static constexpr uint8_t ch[NUM_CHANNELS]{0, 1, 10, 11, 12, 13};
-            adc_channel_cfg channel_cfg{
-                .gain{ADC_GAIN_1},
-                .reference{ADC_REF_INTERNAL},
-                .acquisition_time{ADC_ACQ_TIME_DEFAULT},
-                .differential{0}
-            };
-            for (auto i : ch) {
-                sequence.channels |= BIT(i);
-                channel_cfg.channel_id = i;
-                adc_channel_setup(dev, &channel_cfg);
-            }
-        }
         return dev == nullptr ? -1 : 0;
     }
-    void run() const {
+    void run() {
         if (!device_is_ready(dev))
             return;
         while (true) {
-            adc_read(dev, &sequence);
+            read_all_channels();
             k_msleep(50);
         }
     }
@@ -40,17 +26,32 @@ public:
         return value;
     }
 private:
+    void read_all_channels() {
+        static constexpr uint8_t ch[NUM_CHANNELS]{0, 1, 10, 11, 12, 13};
+        for (auto i : ch) {
+            // Only single channel supported
+            adc_channel_cfg channel_cfg{
+                .gain{ADC_GAIN_1},
+                .reference{ADC_REF_INTERNAL},
+                .acquisition_time{ADC_ACQ_TIME_DEFAULT},
+                .channel_id = i,
+                .differential{0}
+            };
+            adc_channel_setup(dev, &channel_cfg);
+            adc_sequence sequence{
+                .options{nullptr},
+                .channels{BIT(i)},
+                .buffer{&buffer[i]},
+                .buffer_size{sizeof buffer[i]},
+                .resolution{12},
+                .oversampling{0},
+                .calibrate{0}
+            };
+            adc_read(dev, &sequence);
+        }
+    }
     static constexpr int NUM_CHANNELS{6};
     const device *dev{nullptr};
-    adc_sequence sequence{
-        .options{nullptr},
-        .channels{0},
-        .buffer{buffer},
-        .buffer_size{sizeof buffer},
-        .resolution{12},
-        .oversampling{0},
-        .calibrate{0}
-    };
     uint16_t buffer[NUM_CHANNELS];
 } impl;
 
