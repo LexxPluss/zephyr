04bdb8a5fabba154e2315d50583c3f3fe2bd4bc9
Atsunori Saito <sai@yedo.com>
2021-11-05 18:27:38 +0900

    Fixed position of downward sensor.

--- lexxpluss_apps/src/adc_reader.hpp
+++ lexxpluss_apps/src/adc_reader.hpp
@@ -7,8 +7,8 @@ struct adc_reader {
     static void run(void *p1, void *p2, void *p3);
     static int32_t get(int index);
     static k_thread thread;
-    static constexpr int INDEX_DOWNWARD_L{0};
-    static constexpr int INDEX_DOWNWARD_R{1};
+    static constexpr int INDEX_DOWNWARD_R{0};
+    static constexpr int INDEX_DOWNWARD_L{1};
     static constexpr int INDEX_ACTUATOR_0{2};
     static constexpr int INDEX_ACTUATOR_1{3};
     static constexpr int INDEX_ACTUATOR_2{4};
