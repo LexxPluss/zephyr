a63dfd2dd68b62a17121f810168bd3633572dc14
Atsunori Saito <sai@yedo.com>
2021-11-29 12:18:04 +0900

    Added constructor to msg_ros2led class.

--- lexxpluss_apps/src/led_controller.hpp
+++ lexxpluss_apps/src/led_controller.hpp
@@ -1,10 +1,32 @@
 #pragma once
 
 #include <zephyr.h>
+#include <cstring>
 
 struct msg_ros2led {
-    uint32_t pattern, interrupt_ms;
-    uint8_t rgb[3];
+    msg_ros2led() : pattern(NONE), interrupt_ms(0) {}
+    msg_ros2led(uint32_t pattern, uint32_t interrupt_ms) :
+        pattern(pattern), interrupt_ms(interrupt_ms) {}
+    msg_ros2led(const char *str) {
+        if      (strcmp(str, "emergency_stop")  == 0) pattern = EMERGENCY_STOP;
+        else if (strcmp(str, "amr_mode")        == 0) pattern = AMR_MODE;
+        else if (strcmp(str, "agv_mode")        == 0) pattern = AGV_MODE;
+        else if (strcmp(str, "mission_pause")   == 0) pattern = MISSION_PAUSE;
+        else if (strcmp(str, "path_blocked")    == 0) pattern = PATH_BLOCKED;
+        else if (strcmp(str, "manual_drive")    == 0) pattern = MANUAL_DRIVE;
+        else if (strcmp(str, "charging")        == 0) pattern = CHARGING;
+        else if (strcmp(str, "waiting_for_job") == 0) pattern = WAITING_FOR_JOB;
+        else if (strcmp(str, "left_winker")     == 0) pattern = LEFT_WINKER;
+        else if (strcmp(str, "right_winker")    == 0) pattern = RIGHT_WINKER;
+        else if (strcmp(str, "both_winker")     == 0) pattern = BOTH_WINKER;
+        else if (strcmp(str, "move_actuator")   == 0) pattern = MOVE_ACTUATOR;
+        else if (strcmp(str, "charge_level")    == 0) pattern = CHARGE_LEVEL;
+        else if (strcmp(str, "showtime")        == 0) pattern = SHOWTIME;
+        else                                          pattern = NONE;
+        interrupt_ms = 0;
+    }
+    uint32_t pattern{NONE}, interrupt_ms{0};
+    uint8_t rgb[3]{0, 0, 0};
     static constexpr uint32_t NONE{0};
     static constexpr uint32_t EMERGENCY_STOP{1};
     static constexpr uint32_t AMR_MODE{2};
--- lexxpluss_apps/src/rosserial_led.hpp
+++ lexxpluss_apps/src/rosserial_led.hpp
@@ -15,23 +15,7 @@ public:
     void poll() {}
 private:
     void callback_string(const std_msgs::String &req) {
-        msg_ros2led message;
-        if      (strcmp(req.data, "emergency_stop")  == 0) message.pattern = msg_ros2led::EMERGENCY_STOP;
-        else if (strcmp(req.data, "amr_mode")        == 0) message.pattern = msg_ros2led::AMR_MODE;
-        else if (strcmp(req.data, "agv_mode")        == 0) message.pattern = msg_ros2led::AGV_MODE;
-        else if (strcmp(req.data, "mission_pause")   == 0) message.pattern = msg_ros2led::MISSION_PAUSE;
-        else if (strcmp(req.data, "path_blocked")    == 0) message.pattern = msg_ros2led::PATH_BLOCKED;
-        else if (strcmp(req.data, "manual_drive")    == 0) message.pattern = msg_ros2led::MANUAL_DRIVE;
-        else if (strcmp(req.data, "charging")        == 0) message.pattern = msg_ros2led::CHARGING;
-        else if (strcmp(req.data, "waiting_for_job") == 0) message.pattern = msg_ros2led::WAITING_FOR_JOB;
-        else if (strcmp(req.data, "left_winker")     == 0) message.pattern = msg_ros2led::LEFT_WINKER;
-        else if (strcmp(req.data, "right_winker")    == 0) message.pattern = msg_ros2led::RIGHT_WINKER;
-        else if (strcmp(req.data, "both_winker")     == 0) message.pattern = msg_ros2led::BOTH_WINKER;
-        else if (strcmp(req.data, "move_actuator")   == 0) message.pattern = msg_ros2led::MOVE_ACTUATOR;
-        else if (strcmp(req.data, "charge_level")    == 0) message.pattern = msg_ros2led::CHARGE_LEVEL;
-        else if (strcmp(req.data, "showtime")        == 0) message.pattern = msg_ros2led::SHOWTIME;
-        else                                               message.pattern = msg_ros2led::NONE;
-        message.interrupt_ms = 0;
+        msg_ros2led message{req.data};
         while (k_msgq_put(&msgq_ros2led, &message, K_NO_WAIT) != 0)
             k_msgq_purge(&msgq_ros2led);
     }
